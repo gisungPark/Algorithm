d8dd8e418a83d678c33e4b1bfa3dcc8c6f084d04
gisung2215 <wjv2215@naver.com>
2020-08-09 21:08:05 +0900

    Solve: Hanoitop

---
+++ src/JUNGOL1719/BOJ2941.java
@@ -0,0 +1,43 @@
+package JUNGOL1719;
+
+import java.util.Scanner;
+
+//////////BOJ2941. 크로아티아 알파벳 ////////////
+public class BOJ2941 {
+
+	static int cnt;
+	public static void main(String[] args) {
+
+		Scanner sc =new Scanner(System.in);
+		String str = sc.next();
+		
+		int cnt=0;
+		for(int i=0; i<str.length()-2; i++) {
+			String tmp = str.substring(i,i+3);
+			if(tmp.equals("dz=")) cnt++;
+		}
+		str = str.replace("dz=", " ");
+		
+		for(int i=0; i<str.length()-1; i++) {
+			String tmp = str.substring(i,i+2);
+			if(tmp.equals("c=")) cnt++;
+			else if(tmp.equals("c-")) cnt++;
+			else if(tmp.equals("d-")) cnt++;
+			else if(tmp.equals("lj")) cnt++;
+			else if(tmp.equals("nj")) cnt++;
+			else if(tmp.equals("s=")) cnt++;
+			else if(tmp.equals("z=")) cnt++;
+		}
+		str = str.replace("c=", " ").replace("c-", " ")
+		.replace("d-", " ").replace("lj", " ").replace("nj", " ")
+		.replace("s=", " ").replace("z=", " ");
+		
+		
+		for(int i=0; i<str.length(); i++) {
+			if(str.charAt(i) != ' ') cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+
+}

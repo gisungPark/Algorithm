8aa1e2a9899eb5c39003a2a766c2e6b81e2eb845
gisung2215 <wjv2215@naver.com>
2020-08-28 12:42:55 +0900

    Solve: BOJ9663

--- src/com/algorithm03/dfs_bfs/BOJ9663.java
+++ src/com/algorithm03/dfs_bfs/BOJ9663.java
@@ -2,15 +2,39 @@ package com.algorithm03.dfs_bfs;
 
 import java.util.Scanner;
 
-//////////// BOJ9663. N-Queen
+//////////// BOJ9663. N-Queen /////////
 public class BOJ9663 {
 	
-	static int N;
+	static int N, ANS;
+	static int[] rowNum;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
+		rowNum = new int[N];
 		
-		
-		
+		Queen(0);
+		System.out.println(ANS);
+	}
+
+	private static void Queen(int row) {
+		if(row == N) {
+			++ANS;
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(check(row,i)) {
+				rowNum[row] = i;
+				Queen(row+1);
+			}
+		}
+	}
+
+	private static boolean check(int row, int idx) {
+		for(int i=0; i<row; i++) {
+			if(rowNum[i] == idx
+					|| Math.abs(rowNum[i]-idx) == (row-i)) return false;
+		}
+		return true;
 	}
 }
